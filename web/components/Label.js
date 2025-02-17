c6e5b84b73847f64e2b4bd5b89450ea1eb11accb
Roy van de Water <roy.v.water@gmail.com>
2025-02-17 08:51:50 -0500

    Labels should be labels

--- web/components/Label.js
+++ web/components/Label.js
@@ -5,7 +5,7 @@ import { cn } from "../cn.js";
 
 const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 flex flex-col gap-y-1");
 
-const Label = (props) => html` <div ...${props} class=${cn(labelVariants(), props.class)} /> `;
+const Label = (props) => html` <label ...${props} class=${cn(labelVariants(), props.class)} /> `;
 
 Label.displayName = "Label";
 
