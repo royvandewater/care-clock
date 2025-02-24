172317fcbfb48c50358793553acc9d79a3757ef6
Roy van de Water <roy.v.water@gmail.com>
2025-02-24 06:51:08 -0700

    Add service worker to serve static assets offline
    Is currently a passthrough for the POST & PUT requests that
    create & update activities

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request, event) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  const responseFromNetwork = await fetch(request);
+  event.waitUntil(putInCache(request, responseFromNetwork.clone()));
+  return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method === "GET") {
+    return event.respondWith(cacheFirst(event.request, event));
+  }
+
+  return fetch(event.request);
+});
