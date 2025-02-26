4bf1e3effa86228b5e64064647b1036e5b3874f8
Roy van de Water <roy.v.water@gmail.com>
2025-02-26 06:14:53 -0700

    Remove all the tasks related stuff from the Wrangler generator

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,6 @@ import { cors } from "hono/cors";
 import { ActivityCreate } from "endpoints/activityCreate";
 import { ActivityUpdate } from "endpoints/activityUpdate";
 
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
-
 // Start a Hono app
 const app = new Hono();
 
@@ -23,10 +18,6 @@ openapi.use(cors());
 // Register OpenAPI endpoints
 openapi.post("/activities", ActivityCreate);
 openapi.put("/activities/:rowNumber", ActivityUpdate);
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 
 // Export the Hono app
 export default app;
