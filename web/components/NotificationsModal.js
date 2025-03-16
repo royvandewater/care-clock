10639c1977704d3471d4837290a67eb747eb5b02
Roy van de Water <roy.v.water@gmail.com>
2025-03-16 08:06:18 -0700

    Remove unreachable code

--- web/components/NotificationsModal.js
+++ web/components/NotificationsModal.js
@@ -42,29 +42,6 @@ export const NotificationsModal = ({ database, onClose }) => {
       Upload All
     </button>
   </${Modal}>`;
-
-  return html`<div class="fixed top-0 left-0 w-full h-full z-10 backdrop-blur-sm p-4" onClick=${onClose}>
-    <div class="mx-auto max-w-md w-full h-full shadow-lg bg-background p-4 rounded-lg flex flex-col gap-4" onClick=${(e) => e.stopPropagation()}>
-      <header class="text-center relative">
-        <h1 class="text-2xl font-bold">Unsynchronized</h1>
-        <button class="size-8 flex items-center justify-center hover:bg-tertiary-hover rounded-xl absolute top-0 right-0" onClick=${onClose}>
-          <${Close} />
-        </button>
-      </header>
-      <ul class="divide-solid divide-y-1 divide-background-secondary p-4">
-        ${activities.value.map((activity) => html`<${Activity} activity=${activity} />`)}
-        ${activities.value.length === 0 && html`<li class="text-foreground-secondary text-center">All activities are uploaded.</li>`}
-      </ul>
-      <button
-        class="bg-primary text-primary-foreground p-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
-        type="button"
-        onClick=${onSyncAll}
-        disabled=${activities.value.length === 0}
-      >
-        Upload All
-      </button>
-    </div>
-  </div>`;
 };
 
 const Activity = ({ activity }) => {
