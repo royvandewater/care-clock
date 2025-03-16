bebdda565ad0c98ea238fb6da5d25089376ad8b6
Roy van de Water <roy.v.water@gmail.com>
2025-03-16 10:42:04 -0700

    Don't show the camper modal by default

--- web/App.js
+++ web/App.js
@@ -25,7 +25,7 @@ export const App = ({ database }) => {
   const activity = useSignal(parseLocalStorageActivity(window.localStorage.getItem("activity")));
   const isRunning = Boolean(activity.value.startTime);
 
-  const showCamperModal = useSignal(true);
+  const showCamperModal = useSignal(false);
   const showNotificationsModal = useSignal(false);
   const hasNotifications = useSignal(false);
 
