ebc12b2f8eec77b2c5cc837611efb08f6fbef0f5
Roy van de Water <roy.v.water@gmail.com>
2025-03-22 07:01:35 -0700

    Swap the notifications bell for a history icon

--- web/components/NotificationsIndicator.js
+++ web/components/NotificationsIndicator.js
@@ -1,16 +1,13 @@
 import { html } from "htm/preact";
-import { Bell } from "./icons/Bell.js";
-import { BellAlert } from "./icons/BellAlert.js";
+import { History } from "./icons/History.js";
 import { cn } from "../cn.js";
 
 /**
  * @param {{class: string, hasNotifications: Signal<boolean>, onClick: () => void}} props
  */
 export const NotificationsIndicator = ({ hasNotifications, ...props }) => {
-  const Icon = hasNotifications.value ? BellAlert : Bell;
-
   return html` <button class=${cn("size-8 flex items-center justify-center hover:bg-tertiary-hover rounded-xl", props.class)} type="button" onClick=${props.onClick}>
-    ${hasNotifications.value && html`<span class="absolute top-0 right-0 size-2 rounded-full bg-danger"></span>`}
-    <${Icon} />
+    ${hasNotifications.value && html`<span class="absolute top-1.25 right-1.25 size-2 rounded-full bg-danger"></span>`}
+    <${History} class="text-danger" />
   </button>`;
 };
---
+++ web/components/icons/History.js
@@ -0,0 +1,35 @@
+import { html } from "htm/preact";
+
+export const History = () => {
+  return html`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <defs id="defs1">
+      <marker
+        style="overflow:visible"
+        id="RoundedArrow"
+        refX="0"
+        refY="0"
+        orient="auto-start-reverse"
+        markerWidth="0.5"
+        markerHeight="0.5"
+        viewBox="0 0 1 1"
+        preserveAspectRatio="xMidYMid"
+      >
+        <path
+          transform="scale(0.7)"
+          d="m -0.21114562,-4.1055728 6.42229122,3.21114561 a 1,1 90 0 1 0,1.78885438 L -0.21114562,4.1055728 A 1.236068,1.236068 31.717474 0 1 -2,3 v -6 a 1.236068,1.236068 148.28253 0 1 1.78885438,-1.1055728 z"
+          style="fill:context-stroke;fill-rule:evenodd;stroke:none"
+        />
+      </marker>
+    </defs>
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="m 3,12 c 0,-4.9705661 4.0294374,-8.9999961 9,-8.9999961 4.970563,0 9,4.02943 9,8.9999961 0,4.970566 -4.029437,8.999996 -9,8.999996 -2.4852813,0 -4.7352813,-1.007358 -6.363961,-2.636036"
+      id="path1"
+      style="marker-start:url(#RoundedArrow)"
+    />
+    <g id="layer2">
+      <path d="m 11.613783,7.1689485 v 6.0000025 l 3.81614,2.198122" style="display:inline;stroke-linecap:round;stroke-linejoin:round" />
+    </g>
+  </svg> `;
+};
