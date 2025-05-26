c40a870b9d20a37d3e04414787786e9b8c34aabd
Roy van de Water <roy.v.water@gmail.com>
2025-05-26 11:40:25 -0700

    Remove isLocal, it's no longer needed

--- web/data/apiUrl.js
+++ web/data/apiUrl.js
@@ -1,3 +1,1 @@
-import { isLocal } from "./isLocal.js";
-
 export const apiUrl = (path) => new URL(path, window.location.origin);
--- web/data/isLocal.js
+++
@@ -1,1 +0,0 @@
-export const isLocal = () => ["localhost", "127.0.0.1"].includes(location.hostname);
