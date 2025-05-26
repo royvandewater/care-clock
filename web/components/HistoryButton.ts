22b47dda7a2262bf068f708e9ca1d4575d43ca95
Roy van de Water <roy.v.water@gmail.com>
2025-05-26 16:07:29 -0700

    HistoryButton.js -> TS

--- web/components/HistoryButton.ts
+++ web/components/HistoryButton.ts
@@ -1,11 +1,12 @@
 import { html } from "htm/preact";
 import { History } from "./icons/History.js";
 import { cn } from "../cn.js";
+import type { Signal } from "@preact/signals";
 
 /**
  * @param {{class: string, hasNotifications: Signal<boolean>, onClick: () => void}} props
  */
-export const HistoryButton = ({ hasNotifications, ...props }) => {
+export const HistoryButton = ({ hasNotifications, ...props }: { class?: string; hasNotifications: Signal<boolean>; onClick: () => void }) => {
   return html`
     <button type="button" aria-label="History" class=${cn("size-8 flex items-center justify-center hover:bg-tertiary-hover rounded-xl", props.class)} onClick=${props.onClick}>
       ${hasNotifications.value && html`<span class="absolute top-0.5 right-0.5 size-2 rounded-full bg-danger"></span>`}
