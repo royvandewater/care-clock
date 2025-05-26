476420c5d92aabbd0c4b3940e60954cc27bddaf3
Roy van de Water <roy.v.water@gmail.com>
2025-05-26 10:48:34 -0700

    Migrate to using non-dev tailwindcss

---
+++ web/index.ts
@@ -0,0 +1,18 @@
+import { render } from "preact";
+import { html } from "htm/preact";
+import { registerServiceWorker } from "./registerServiceWorker.js";
+import { App } from "./App.js";
+import { connectToDatabase } from "./data/database.js";
+import { assert } from "@/assert";
+
+import "./index.css";
+
+(async () => {
+  const database = await connectToDatabase();
+  const app = document.getElementById("app");
+  assert(app, "App element not found");
+
+  render(html`<${App} database=${database} />`, app!);
+
+  registerServiceWorker();
+})();
