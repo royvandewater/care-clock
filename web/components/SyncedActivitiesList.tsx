aff67bb56e6952e858a4559dc3d344030c55fa04
Roy van de Water <roy.v.water@gmail.com>
2025-06-08 14:42:06 -0700

    Migrate SyncedActivitiesList -> TSX

--- web/components/SyncedActivitiesList.tsx
+++ web/components/SyncedActivitiesList.tsx
@@ -1,4 +1,3 @@
-import { html } from "htm/preact";
 import { useSignal } from "@preact/signals";
 import { useEffect } from "preact/hooks";
 import { getSyncedActivities } from "@/data/database";
@@ -6,7 +5,13 @@ import { Edit } from "@/components/icons/Edit";
 import { Button } from "@/components/Button";
 import type { Activity } from "@/data/serialization.js";
 
-export const SyncedActivitiesList = ({ database, onEditActivity }: { database: IDBDatabase; onEditActivity: (id: string) => void }) => {
+export const SyncedActivitiesList = ({
+  database,
+  onEditActivity,
+}: {
+  database: IDBDatabase;
+  onEditActivity: (id: string) => void;
+}) => {
   const activities = useSignal<Activity[]>([]);
 
   useEffect(() => {
@@ -19,24 +24,32 @@ export const SyncedActivitiesList = ({ database, onEditActivity }: { database: I
     return () => database.removeEventListener("activities:changed", updateActivities);
   }, [database]);
 
-  return html` <div class="flex flex-col gap-y-4">
-    <h2 class="text-center text-lg font-bold">Past Activities</h2>
-    <ul class="divide-solid divide-y-1 divide-input-border">
-      ${activities.value.map((activity) => html`<${ActivityRow} activity=${activity} onClickEdit=${onEditActivity} />`)}
-    </ul>
-  </div>`;
+  return (
+    <div class="flex flex-col gap-y-4">
+      <h2 class="text-center text-lg font-bold">Past Activities</h2>
+      <ul class="divide-solid divide-y-1 divide-input-border">
+        {activities.value.map((activity) => (
+          <ActivityRow activity={activity} onClickEdit={onEditActivity} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 const ActivityRow = ({ activity, onClickEdit }: { activity: Activity; onClickEdit: (id: string) => void }) => {
   const startTime = activity.startTime ? new Date(activity.startTime).toLocaleString() : "UNKNOWN";
 
-  return html`<li>
-    <div class="flex justify-between items-center py-2" onClick=${() => onClickEdit(activity.id)}>
-      <div>
-        <h2 class="text-sm">${activity.camperName}</h3>
-        <h3 class="text-xs text-foreground-secondary">${startTime}</h2>
+  return (
+    <li>
+      <div class="flex justify-between items-center py-2" onClick={() => onClickEdit(activity.id)}>
+        <div>
+          <h2 class="text-sm">{activity.camperName}</h2>
+          <h3 class="text-xs text-foreground-secondary">{startTime}</h3>
+        </div>
+        <Button variant="tertiary" size="xs" type="button">
+          <Edit />
+        </Button>
       </div>
-      <${Button} variant="tertiary" size="xs" type="button"><${Edit} /></${Button}>
-    </div>
-  </li>`;
+    </li>
+  );
 };
