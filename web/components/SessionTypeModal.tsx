cd3b5efc351e095cdb2ee48b637382df3e776033
Roy van de Water <roy.v.water@gmail.com>
2025-06-08 14:37:51 -0700

    Migrate SessionTypeModal -> TSX

--- web/components/SessionTypeModal.tsx
+++ web/components/SessionTypeModal.tsx
@@ -5,30 +5,51 @@ import { Button } from "@/components/Button";
 
 import { sessionTypes, type SessionType } from "@/data/sessionTypes";
 
-export const SessionTypeModal = ({ onClose, onSelect }: { onClose: () => void; onSelect: (sessionType: SessionType) => void }) => {
-  const onSelectSessionType = (sessionType: SessionType) => {
+export const SessionTypeModal = ({
+  onClose,
+  onSelect,
+}: {
+  onClose: () => void;
+  onSelect: (sessionType: SessionType) => void;
+}) => {
+  const onSelectSessionType = (event: Event, sessionType: SessionType) => {
+    event.stopPropagation();
+    event.preventDefault();
+
     onSelect(sessionType);
     onClose();
   };
 
-  return html`<${Modal} title=${html`<${Header} />`} onClose=${onClose}>
-    <ul class="flex flex-col divide-y-1 divide-secondary/40">
-      ${sessionTypes.map(
-        (sessionType) => html`<li>
-          <div class="flex justify-between items-center p-2" onClick=${() => onSelectSessionType(sessionType)}>
-            <span>${sessionType}</span>
-            <${SelectButton} sessionType=${sessionType} />
-          </div>
-        </li>`,
-      )}
-    </ul>
-  </${Modal}>`;
+  return (
+    <Modal title={<Header />} onClose={onClose}>
+      <ul class="flex flex-col divide-y-1 divide-secondary/40">
+        {sessionTypes.map((sessionType) => (
+          <li>
+            <div
+              class="flex justify-between items-center p-2"
+              onClick={(event: Event) => onSelectSessionType(event, sessionType)}
+            >
+              <span>{sessionType}</span>
+              <SelectButton
+                sessionType={sessionType}
+                onClick={(event: Event) => onSelectSessionType(event, sessionType)}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Modal>
+  );
 };
 
 const Header = () => {
-  return html` <h1 class="text-2xl font-bold">Session Type</h1> `;
+  return <h1 class="text-2xl font-bold">Session Type</h1>;
 };
 
-const SelectButton = ({ onClick, sessionType }) => {
-  return html`<${Button} type="button" onClick=${onClick} size="xs" aria-label=${`Select ${sessionType}`}>Select</${Button}>`;
+const SelectButton = ({ onClick, sessionType }: { onClick: (event: Event) => void; sessionType: SessionType }) => {
+  return (
+    <Button type="button" onClick={onClick} size="xs" aria-label={`Select ${sessionType}`}>
+      Select
+    </Button>
+  );
 };
