dfc3f977637b08e61f01bac144bbd229f578b76d
Roy van de Water <roy.v.water@gmail.com>
2025-06-08 14:27:58 -0700

    HistoryButton -> TSX

--- web/components/HistoryButton.tsx
+++ web/components/HistoryButton.tsx
@@ -1,16 +1,23 @@
-import { html } from "htm/preact";
 import { History } from "@/components/icons/History";
 import { cn } from "@/cn";
 import type { Signal } from "@preact/signals";
 
-/**
- * @param {{class: string, hasNotifications: Signal<boolean>, onClick: () => void}} props
- */
-export const HistoryButton = ({ hasNotifications, className, ...props }: { className?: string; hasNotifications: Signal<boolean>; onClick: () => void }) => {
-  return html`
-    <button type="button" aria-label="History" class=${cn("size-8 flex items-center justify-center hover:bg-tertiary-hover rounded-xl", className)} onClick=${props.onClick}>
-      ${hasNotifications.value && html`<span class="absolute top-0.5 right-0.5 size-2 rounded-full bg-danger"></span>`}
-      <${History} className="text-danger" />
-    </button>
-  `;
-};
+export const HistoryButton = ({
+  hasNotifications,
+  className,
+  ...props
+}: {
+  className?: string;
+  hasNotifications: Signal<boolean>;
+  onClick: () => void;
+}) => (
+  <button
+    type="button"
+    aria-label="History"
+    class={cn("size-8 flex items-center justify-center hover:bg-tertiary-hover rounded-xl", className)}
+    onClick={props.onClick}
+  >
+    {hasNotifications.value && <span class="absolute top-0.5 right-0.5 size-2 rounded-full bg-danger"></span>}
+    <History className="text-danger" />
+  </button>
+);
