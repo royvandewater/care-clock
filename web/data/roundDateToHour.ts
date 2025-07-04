16414035e061088e1928bbb7bbe0e8382419fdd6
Roy van de Water <roy.v.water@gmail.com>
2025-07-04 10:39:37 -0800

    Round date down

--- web/data/roundDateToHour.ts
+++ web/data/roundDateToHour.ts
@@ -1,3 +1,3 @@
 export const roundDateToHour = (date: Date) => {
-  return new Date(Math.round(date.getTime() / 3600000) * 3600000);
+  return new Date(Math.floor(date.getTime() / 3600000) * 3600000);
 };
